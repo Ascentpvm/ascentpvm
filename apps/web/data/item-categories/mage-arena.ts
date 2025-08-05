9a32bb68eac10d60f4ee6e02d8a6417ee45b1ed5
Ascentpvm <Ascentpvmwebsite@gmail.com>
2025-08-05 13:07:55 -0400

    Update mage-arena.ts

--- apps/web/data/item-categories/mage-arena.ts
+++ apps/web/data/item-categories/mage-arena.ts
@@ -1,1 +1,17 @@
+import { formatWikiImageUrl } from '@/app/rank-calculator/utils/format-wiki-url';
+import { ItemCategory } from '@/app/schemas/items';
+import { compoundItem, singleItem } from '../utils/item-builders';
 
+export const magicTrainingArena: ItemCategory = {
+  image: formatWikiImageUrl('Mage Training Arena Logo', 'category'),
+  items: [
+    singleItem({
+      name: 'Master wand',
+      collectionLogCategory: 'Magic_training_arena',
+    }),
+    singleItem({
+      name: 'Infinity boots',
+      collectionLogCategory: 'Magic_training_arena',
+    }),
+  ],
+};
